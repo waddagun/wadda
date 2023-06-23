7bec8cd8101d1f0a33635e588f0786a1a2a5dfa9
waddaguma <xanux@naver.com>
2023-06-23 17:28:33 +0900

    first commit

---
+++ src/main/java/com/pcm/prj05/Prj05Application.java
@@ -0,0 +1,13 @@
+package com.pcm.prj05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj05Application.class, args);
+	}
+
+}
